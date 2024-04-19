e1e1e83c9ed70dd68467279b0edde2157f4c011e
arvindmeena0033 <146611810+arvindmeena0033@users.noreply.github.com>
2024-04-20 03:05:44 +0530

    Update login.php

--- PHP_Test_NEC/login.php
+++ PHP_Test_NEC/login.php
@@ -1,7 +1,7 @@
 <html>
 <head>
   <title>Login Page</title>
-    <link rel="stylesheet" href="CSS/style.css" type="text/css"> //adding stylesheet
+    <link rel="stylesheet" href="CSS/style.css" type="text/css"> 
 </head>
 <body>
   <div class="login-heading">
